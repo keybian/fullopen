bda668c14dd5edc65c6c8312f3e12651c2401883
keybian <keybian@gmail.com>
2026-02-15 07:28:16 -0400

    paso 3 agregando numeros de telefono

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,11 +4,15 @@ import Person from './components/Person'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    {
+      name: 'Arto Hellas',
+      number: '040-1234567'
+    }
   ])
   console.log('person', persons)
 
   const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -17,11 +21,13 @@ const App = () => {
     console.log(findName)
     if (findName === undefined) {
       const newObject = {
-        name: newName
+        name: newName,
+        number: newPhone
       }
 
       setPersons(persons.concat(newObject))
       setNewName('')
+      setNewPhone('')
     } else {
       alert(`${newName}  is already added to phonebook`)
     }
@@ -32,24 +38,32 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    setNewPhone(event.target.value)
+  }
+
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <ul>
-        {persons.map(person =>
-          <Person key={person.name} person={person} />
-        )}
-      </ul>
+
       <form onSubmit={addPerson}>
         <div>
           name:<input value={newName} onChange={handlePersonChange} />
+        </div>
+        <div>
+          number:<input value={newPhone} onChange={handleNumberChange} />
         </div>
         <div>
           <button type='submit'>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      <ul>
+        {persons.map(person =>
+          <Person key={person.name} person={person} />
+        )}
+      </ul>
       <div>debug: {newName}</div>
     </div>
   )
